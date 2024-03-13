1811c33ab34fb04e7d8c19d5b6a16f386e5457ef
naruepol <naruapon@gmail.com>
2024-03-14 01:42:20 +0700

    Rename Title

--- src/BMICalculatorComponent.tsx
+++ src/BMICalculatorComponent.tsx
@@ -26,7 +26,7 @@ function BMICalculatorComponent() {
 
   return (
     <div>
-      <h1>BMI Calculator</h1>
+      <h1>TS BMI Calculator</h1>
       <div>
         <label>Weight (kg):</label>
         <input
