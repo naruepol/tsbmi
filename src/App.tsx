c13d14f0652d112b10d440aa4c85fbcb6a3bffd3
naruepol <naruapon@gmail.com>
2024-03-14 01:35:09 +0700

    2nd commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import BMICalculatorComponent from "./BMICalculatorComponent";
+
+function App() {
+  return (
+    <div className="App">
+      <BMICalculatorComponent />
+    </div>
+  );
+}
+
+export default App;
